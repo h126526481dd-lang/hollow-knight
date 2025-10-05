0ad9804bb4d7fbba9d174528f5c12852c6cfdad6
WS_kanata <whitesheep901229@gmail.com>
2025-10-05 15:10:36 +0800

    10/5_15:10

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ import math
 import tool
 import sys
 
+
+
 #=======================================================================================================
+
+
 
 pygame.init()                                                   #初始化
 Info = pygame.display.Info()                                      #偵測用戶顯示參數
@@ -25,12 +29,13 @@ FPS = 60                                                     #設定每秒幀數
 
 Main = player_class.player("BOBO",0,0)                #建立角色物件
 
+# font = pygame.font.Font(None, 50)
+
 
 print(pygame.display.get_active())                              #確認是否正確開啟
 
-scene_ctrl = 10
+scene_ctrl = 0
 pre_keys = []
-
 #=======================================================================================================
 
 while True:
@@ -38,20 +43,29 @@ while True:
         
         case 0:                                                              #場景0
             
-            while True: 
+            while(1==1): 
                 screen.fill(color= "white")
-                
-                test=button.button("black",200,200)
-                
-                screen.blit(test.image,(400,400))
-                
                 pygame.display.update()
 
+#                button1 = button(200, 100, "hello")
+#                button1 = pygame.sprite.Group()
+
+#                pygame.sprite.Group.draw()
+
+#                text_surf = pygame.font.Font.render("hello", True, (255, 255, 255))
+#                text_rect = text_surf.get_rect(center=(Info.current_h/2, Info.current_w/2))
+
+                
                 for event in pygame.event.get():                               #偵測事件
                     if event.type == pygame.QUIT:
                         pygame.quit()
                         exit()
             
+
+
+
+
+
 #=======================================================================================================
 
         case 10:                                                             #場景10
@@ -64,27 +78,16 @@ while True:
             ATKs_EN = []
 
             scene.append(pygame.image.load("IMG_2794.jpg"))                  #導入背景圖片
-            scene[0] = pygame.transform.scale(scene[0], (screen_width*8, screen_height*8))  # 調整大小
+            scene[0] = pygame.transform.scale(scene[0], (screen_width*5, screen_height*5))  # 調整大小
             
             NT_object.append(object_class.object(1200,800,tool.HRZ_combine("floor.png",10),"wall",0,0,0,0,0))
             NT_object.append(object_class.object(-50,400,tool.HRZ_combine("floor.png",10),"wall",0,0,0,1,0))
-            NT_object.append(object_class.object(1200,0,tool.HRZ_combine("floor.png",10),"wall",0,0,0,1,0))
-            NT_object.append(object_class.object(-50,-400,tool.HRZ_combine("floor.png",10),"wall",0,0,0,1,0))
-            NT_object.append(object_class.object(1200,-800,tool.HRZ_combine("floor.png",10),"wall",0,0,0,1,0))
-            NT_object.append(object_class.object(-50,-1200,tool.HRZ_combine("floor.png",10),"wall",0,0,0,1,0))
-            NT_object.append(object_class.object(1200,-1600,tool.HRZ_combine("floor.png",10),"wall",0,0,0,1,0))
-
 
 
             door = pygame.image.load("door.png")
             door = pygame.transform.scale(door, (200, 200))  # 調整大小
-            
-            CT_object.append(object_class.object(2400,600,door,"path",0,0,0,0,0))
-            
             CT_object.append(object_class.object(1600,500,door,"door",0,0,0,0,0))
-            CT_object.append(object_class.object(2000,-2000,pygame.image.load("skill.png"),"skill",0,0,0,6,0))
-            CT_object.append(object_class.object(2000,600,pygame.image.load("skill.png"),"skill",0,0,0,4,0))
-
+            CT_object.append(object_class.object(2000,300,pygame.image.load("skill.png"),"skill",0,0,0,6,0))
 
             Enemy.append(player_class.enemy("The_First",1200,0,100,"zombie"))
             
@@ -96,25 +99,23 @@ while True:
                     Main.is_hurt -= 1
                     continue
                     
-                #print("FPS:", clock.get_fps())
+               # print("FPS:", clock.get_fps())
                 
                 keys = pygame.key.get_pressed()                             #偵測按鍵(把偵測按鍵拉出event.get()迴圈外，規避windows的按鍵延遲)
 
-                for obj in CT_object:
-                    if keys[pygame.K_w]:                                #按下w進門
-
+                if keys[pygame.K_w]:                                #按下w進門
+                    for obj in CT_object:
                         if obj.type == "door":
                             if tool.Touch(Main,obj):
-                                scene_ctrl = 11
-                    if obj.type == "path":
-                        if tool.Touch(Main,obj):
-                            scene_ctrl = 0
-
+                                scene_ctrl=11
                                 
                 tool.tick_mission(screen, scene, Main, Enemy, ATKs_AL, ATKs_EN, NT_object, CT_object, keys, pre_keys)
 
                 pre_keys = keys
-                
+
+
+                                
+          
 #=======================================================================================================
 
         case 11:                                                             #場景1
